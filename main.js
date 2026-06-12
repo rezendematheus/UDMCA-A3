8e268e35bb4dabcf6d9edc8bdd491875d7f4b0b1
rezendematheus <matheusreisrezende.2002@gmail.com>
2026-06-11 23:35:00 -0300

    feat: esquema de favoritos em localStorage com estilização

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 let templateCard = `
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border p-4 produto">
+        <div id="prod-{{id}}" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border p-4 produto">
             <div class="card">
-                <img src="{{thumbnail}}" class="card-img-top" alt="" style="max-height: 100px;">
+                <img src="{{thumbnail}}" class="card-img-top thumbnail" alt="" style="max-height: 100px;">
                 <div class="card-body" style="min-height: 160px;">
-                    <div class="fw-bold text-truncate" >{{ title }} {{ name.first }} {{ name.last }}</div>
+                    <div class="fw-bold text-truncate titulo" >{{ title }}</div>
                     <div class="fw-light" style="font-size: 10pt">{{ email }}</div>
-                    <p class="text-secondary fs-6">
+                    <p class="text-secondary fs-6 categoria">
                         {{ category }}
                     </p>
                     
-                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-between align-items-center preco">
                         <p class="fw-bold fs-6" style="margin-bottom: 0px;">
                             R$ {{ price }}
                         </p>
-                        <button href="#" class="btn btn-outline-secondary btn-sm">
+                        <button data-id="{{ id }}" href="#" class="btn btn-sm btn-favoritar {{ classeFavorito }}">
                             <iconify-icon icon="lucide:users" class="me-1"></iconify-icon>
-                            Favoritar
+                            {{#if isFavorito}}Favoritado{{else}}Favoritar{{/if}}
                         </button>
                     </div>
                 </div>
@@ -23,18 +23,34 @@ let templateCard = `
         </div>
 `
 
+// Cria o localStorage de favoritos
 let template = Handlebars.compile(templateCard);
 
-
-async function lerDados() {
+// Coleta dos dados da API
+async function lerProdutos() {
     let response = await fetch('https://dummyjson.com/products?limit=25')
     //console.log("response", response);
     let data = await response.json()
     //console.log("data", data)
     let elem = document.getElementById("ProductsList")
     let contentHtml = ""
+    const favoritadosNoStorage = JSON.parse(localStorage.getItem('favoritos')) || [];
     for (let idx in data.products) {
         let product = data.products[idx]
+        let achouFavorito = false;
+        for (let f = 0; f < favoritadosNoStorage.length; f++) {
+            if (String(favoritadosNoStorage[f].id) === String(product.id)) {
+                achouFavorito = true;
+                break; 
+            }
+        }
+
+        if (achouFavorito) {
+            product.classeFavorito = "favoritado btn-secondary";
+        } else {
+            product.classeFavorito = "btn-outline-secondary";
+        }
+
         // console.log(product);
         contentHtml = contentHtml + template(product);
     }
@@ -43,26 +59,20 @@ async function lerDados() {
     elem.innerHTML = contentHtml
 }
 
-async function lerProdutos() {
-    let response = await fetch('https://dummyjson.com/products')
-    //console.log("response", response);
-    let data = await response.json()
-    // console.log("products", data)
-}
+lerProdutos()
 
-lerDados()
-// lerProdutos()
+// Filtragem de produtos 
 
 const inputBusca = document.getElementById("search-input")
 console.log(inputBusca)
 inputBusca.addEventListener('input', (event) => {
     let listaProdutos = document.querySelectorAll(".produto")
-    const userText = event.target.value.toLocaleLowerCase().trim();
+    const inputUser = event.target.value.toLocaleLowerCase().trim();
     
     listaProdutos.forEach(produto => {
         const textoProduto = produto.textContent.toLocaleLowerCase();
         
-        if (!textoProduto.includes(userText)){
+        if (!textoProduto.includes(inputUser)){
             produto.classList.add('collapse');
         } else {
             produto.classList.remove('collapse');
@@ -70,10 +80,74 @@ inputBusca.addEventListener('input', (event) => {
     })
 
     console.log(listaProdutos);
-    
-    
-    // Update the UI or fall back to default text if empty
 });
+
+
+///// Favoritar produtos
+
+const favoritos = []
+
+function guardaFavorito(dadosDoProduto) {
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    favoritos.push(dadosDoProduto)
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+}
+
+function removeFavorito(dadosDoProduto) {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    favoritos = favoritos.filter(favorito => {
+        return favorito.id !== dadosDoProduto.id
+    })
+    
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+}
+
+function encontraProdutoPorId( idDoProduto ) {
+    const produto = document.getElementById("prod-"+idDoProduto)
+
+    const thumbnailProduto =  produto.querySelector(".thumbnail").src;
+    const tituloProduto = produto.querySelector(".titulo").textContent;
+    const precoProduto = produto.querySelector(".preco").textContent;
+    const categoryProduto = produto.querySelector(".categoria").textContent;
+
+    const dadosDoProduto = {
+        id: idDoProduto,
+        thumbnail: thumbnailProduto,
+        title: tituloProduto,
+        price: precoProduto,
+        category: categoryProduto
+    }
+
+    return dadosDoProduto
+}
+
+
+document.getElementById("ProductsList").addEventListener('click', (event) => {
+
+    const botao = event.target.closest('.btn-favoritar');
+    if (!botao) return;
+    
+    event.preventDefault();
+    const idDoProduto  = botao.dataset.id;
+    const dadosDoProduto = encontraProdutoPorId( idDoProduto )
+    if(botao.classList.contains('favoritado')){
+        botao.classList.remove('favoritado');
+        botao.classList.remove('btn-secondary');
+        botao.classList.add('btn-outline-secondary');
+        removeFavorito( dadosDoProduto )
+    } else {
+        botao.classList.add('favoritado');
+        botao.classList.remove('btn-outline-secondary');
+        botao.classList.add('btn-secondary');
+        guardaFavorito( dadosDoProduto )
+    }
+
+    console.log(localStorage.getItem('favoritos'))
+})
+
+
+
+// Criação de uma função que usa o localStorage para os produtos favoritados
 
 
 
